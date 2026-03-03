1566997984403de3ac998fe55ed7a99b1bfee185
Shubhamm-sen <165513090+Shubhamm-sen@users.noreply.github.com>
2026-03-03 15:52:34 +0530

    Enable asynchronous processing in application

--- backend/src/main/java/com/stockarena/StockArenaApplication.java
+++ backend/src/main/java/com/stockarena/StockArenaApplication.java
@@ -2,8 +2,10 @@ package com.stockarena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StockArenaApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockArenaApplication.class, args);
